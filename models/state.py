69e211ca2ee545bc7a1b55b31359e8a76efe1dc1
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-08-13 09:59:37 -0700

    0x00. AirBnB clone - The console

--- models/state.py
+++ models/state.py
@@ -3,5 +3,4 @@ from models.base_model import BaseModel
 class State(BaseModel):
     name = ""
     def __init__(self, *args, **kwargs):
-        """init the state"""
         super().__init__(*args, **kwargs)
